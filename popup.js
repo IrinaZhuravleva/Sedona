48d29b935c31acd92664717d53864eeecec79a2a
IrinaZhuravleva <sparrow113@rambler.ru>
2017-04-16 17:13:34 +0300

    Добавление Sedona-768

--- popup.js
+++ popup.js
@@ -1,20 +1,12 @@
- var link = document.querySelector(".btn-transparent");
+ var link = document.querySelector(".btn-contacts");
   var popup = document.querySelector(".modal-content");
   var close = popup.querySelector(".modal-content-close");
-  var form = popup.querySelector("form");
-  var login = popup.querySelector("[name=login]");
-  var email = popup.querySelector("[name=email]");
-  var storage = localStorage.getItem("login");
+  
 
   link.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.add("modal-content-show");
-    if (storage) {
-      login.value = storage;
-      email.focus();
-    } else {
-      login.focus();
-    }
+  
   });
 
   close.addEventListener("click", function(event) {
@@ -22,14 +14,6 @@
     popup.classList.remove("modal-content-show");
   });
 
-  form.addEventListener("submit", function(event) {
-    if (!login.value || !email.value) {
-      event.preventDefault();
-      console.log("Нужно ввести логин и e-mail");
-    } else {
-      localStorage.setItem("login", login.value);
-    }
-  });
 
     window.addEventListener("keydown", function(event) {
     if (event.keyCode === 27) {
